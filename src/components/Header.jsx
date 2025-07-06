2c8628c44db7924c6b41551534baf4986af2279a
murad-dabbous <moradyousef04@gmail.com>
2025-07-06 19:15:54 +0000

    fix(header): adjust header stlyling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import logo from "../assets/logo1.jpg";
+
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
+        <img src={logo} alt="Site logo" />
         <nav>
           <ul>
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink to="/" className="links">
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about">About</NavLink>
+              <NavLink to="/about" className="links">
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink to="/contact" className="links">
+                Contact
+              </NavLink>
             </li>
           </ul>
         </nav>
-        <img src="logo.png" alt="Site logo" />
-        <h1>
+        {/* <h1>
           <span id="Welcome_to_my_site">Hello, I am Murad.</span>
         </h1>
-        <h2>I am a Frontend/Full-stack Engineer</h2>
+        <h2>I am a Frontend & Full-stack Engineer</h2> */}
       </div>
     </header>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1>
+        <img src="" alt="personal picture" />
+        <span id="Welcome_to_my_site">Hello, I am Murad.</span>
+      </h1>
+      <h2>I am a Frontend/Full-stack Engineer</h2>
+    </div>
+  );
+};
+
+export default Hero;
