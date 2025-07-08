f8973d6ae88214d355b5801438151a38a5e568cf
murad-dabbous <moradyousef04@gmail.com>
2025-07-08 20:08:36 +0000

    feat(projects data): add the data for the projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 const App = () => {
   return (
     <div className="app">
       <Main>
         <Header />
         <Hero />
+        <Projects />
         <Routes>
           {/* <Route path="index" element={<App />} />
           <Route path="/about" element={<About />} />
---
+++ src/components/Projects.jsx
@@ -0,0 +1,7 @@
+import data from "../../data/projectsData.json";
+const Projects = () => {
+  console.log(data);
+  return <div></div>;
+};
+
+export default Projects;
