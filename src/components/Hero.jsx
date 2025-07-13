bdb0540b62065d4d1425527bf6af4d2d178a831f
murad-dabbous <moradyousef04@gmail.com>
2025-07-13 13:39:41 +0000

    fix(hero): modified the hero section's description and picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
 import "./Hero.css";
-import me from "../assets/me.png";
+import me from "../assets/murad.jpg";
 const Hero = () => {
   return (
     <section className="hero" id="about">
       <div className="hero-content">
         <div className="hero-text">
           <h1>
-            Hello, I am <span>Murad Dabbous</span>.
+            Hi, I am <span>Murad Dabbous</span>
           </h1>
           <h2>I am a Frontend & Full-stack Engineer</h2>
           <p>
-            I build responsive, clean, and efficient web applications using
-            modern tools like React, Node, and PostgreSQL.
+            I build responsive, clean, and efficient user interfaces and
+            full-stack web applications using modern tools like React, Node.js,
+            and PostgreSQL.
           </p>
         </div>
         <div className="hero-image">
